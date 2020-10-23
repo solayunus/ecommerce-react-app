9482fab1246e48a9ea9a5e9d503ed3d16a147ef7
Yunus O. KAREEM <solayunus19@gmail.com>
2020-10-22 21:52:16 -0700

    adding header with link to hompage and shopage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Switch } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/header/header-component';
 import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.components';
 
@@ -9,12 +10,12 @@ import ShopPage from './components/pages/shop/shop.components';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
       </Switch>
       
-
     </div>
   );
 }
